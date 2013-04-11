f1e3cd33e0a9a2469fbdd43b31727ad4c7a8ab53
Alexander Weiler <alexander.weiler@gmail.com>
2013-04-11 15:38:09 +0200

    level6 done

--- lex-beginner/player.rb
+++ lex-beginner/player.rb
@@ -1,25 +1,52 @@
 class Player
-  def play_turn(warrior)
+def play_turn(warrior)
 
-  if warrior.feel.empty?
-  	#nothing in front
-    if ((warrior.health < 20) and (@health <= warrior.health))
-        warrior.rest!
-    else
-       warrior.walk!
-    end
-  else
-    #something in front
-    if warrior.feel.captive? 
-    	warrior.rescue!
-    else
-      warrior.attack!
-    end
-  end
-  @health = warrior.health
-  end
+@direction ||= :backward	
+
+if warrior.feel(@direction).wall? 
+  @direction = flip(@direction)
 end
 
+if warrior.feel(@direction).empty?
+  	#nothing in direction
+    if ((warrior.health < 20) and (@health <= warrior.health))
+    	# low health and not hit
+        warrior.rest!
+    else
+    	if warrior.health > 10
+          warrior.walk!(@direction)
+        else
+          warrior.walk!(flip(@direction))
+        end
+    end
+else
+    #something in direction
 
- 
+    if warrior.feel(@direction).captive? 
+    	warrior.rescue!(@direction)
+    else
+      warrior.attack!(@direction)
+    end
+end
+@health = warrior.health
+end #end play_turn
+
+def flip(direction)
+  case 
+  when direction == :forward
+  	direction = :backward
+  when direction == :backward
+  	direction = :forward
+  end 
+end #end flip
+
+
+end  #class
+
+
+
+
+
+#warrior.feel(:backward).wall?
+
  
