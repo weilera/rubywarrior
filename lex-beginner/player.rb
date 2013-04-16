7afa0264c503701f3ee6705a188db0aff5d1b9ae
Alexander Weiler <alexander.weiler@gmail.com>
2013-04-16 15:29:55 +0200

    level 7 done

--- lex-beginner/player.rb
+++ lex-beginner/player.rb
@@ -1,33 +1,34 @@
 class Player
-def play_turn(warrior)
+  def play_turn(warrior)
 
-
-
-if warrior.feel.empty?
-  	#nothing in direction
-    if ((warrior.health < 20) and (@health <= warrior.health))
-    	# low health and not hit
-        warrior.rest!
+  if warrior.feel.empty?
+    puts "#nothing in direction"
+    if ((warrior.health < 20) && (@health <= warrior.health))
+      puts  "low health and not hit"
+      warrior.rest!
     else
-    	if warrior.health > 10
-          warrior.walk!
-        else
-          warrior.walk!
-        end
+      if warrior.health > 12
+        puts  "high health and hit"
+        warrior.walk!
+      else
+        puts  "low health and hit"
+        warrior.walk!(:backward)
+      end
     end
-else
-    #something in direction
+  else
+    puts "#something in direction"
     if warrior.feel.wall? 
-       warrior.pivot!
-    end
-    if warrior.feel.captive? 
-    	warrior.rescue!
+     puts "#felt wall"
+     warrior.pivot!
+    elsif warrior.feel.captive? 
+      puts "#felt captive"
+      warrior.rescue! 
     else
       #default ATTACK!!
       warrior.attack!
     end
-end
-@health = warrior.health
+  end
+  @health = warrior.health
 end #end play_turn
 
 
@@ -35,9 +36,5 @@ end #end play_turn
 end  #class
 
 
-
-
-
 #warrior.feel(:backward).wall?
 
- 
